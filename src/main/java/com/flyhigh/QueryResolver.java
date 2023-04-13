129983aec414193b12d1eef02da5c7d3aae12acc
The-Debarghya <debarghya4444@gmail.com>
2023-04-13 19:14:12 +0530

    remove auth for queries

--- src/main/java/com/flyhigh/QueryResolver.java
+++ src/main/java/com/flyhigh/QueryResolver.java
@@ -3,7 +3,6 @@ package com.flyhigh;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.graphql.data.method.annotation.*;
-import org.springframework.security.access.annotation.Secured;
 
 import java.util.*;
 import java.util.stream.Stream;
@@ -13,37 +12,31 @@ public class QueryResolver {
     @Autowired
     private FlightRepository flightRepository;
 
-    @Secured({ "ROLE_USER", "ROLE_ADMIN" })
     @QueryMapping(name = "flights")
     public List<Flight> flights() {
         return flightRepository.findAll();
     }
 
-    @Secured({ "ROLE_USER", "ROLE_ADMIN" })
     @QueryMapping(name = "flight")
     public Flight flight(@Argument Integer id) {
         return flightRepository.findById(id).orElse(null);
     }
 
-    @Secured({ "ROLE_USER", "ROLE_ADMIN" })
     @QueryMapping
     public List<Flight> flightsByStart(@Argument String start) {
         return flightRepository.findByStart(start);
     }
 
-    @Secured({ "ROLE_USER", "ROLE_ADMIN" })
     @QueryMapping
     public List<Flight> flightsByDest(@Argument String destination) {
         return flightRepository.findByDestination(destination);
     }
 
-    @Secured({"ROLE_USER", "ROLE_ADMIN"})
     @QueryMapping
     public List<Flight> flightsByCost(@Argument Double cost) {
         return flightRepository.findByCostLessThanEqual(cost);
     }
 
-    @Secured({ "ROLE_USER", "ROLE_ADMIN" })
     @QueryMapping
     public List<Flight> flightsByStartAndDest(@Argument String start, @Argument String destination) {
         List<Flight> list1 = flightRepository.findByStartAndDestination(start, destination);
