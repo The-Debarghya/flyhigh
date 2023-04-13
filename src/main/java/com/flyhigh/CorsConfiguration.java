faf504d75255e2920a3ee034135fbfa4c843c3fe
The-Debarghya <debarghya4444@gmail.com>
2023-04-14 00:37:49 +0530

    Add CORS support

---
+++ src/main/java/com/flyhigh/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package com.flyhigh;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/graphql")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("POST") // Allow only POST method
+                .allowedHeaders("*") // Allow any headers
+                .allowCredentials(true); // Allow sending credentials (e.g., cookies)
+
+        registry.addMapping("/api/auth/authenticate")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("POST") // Allow only POST method
+                .allowedHeaders("*") // Allow any headers
+                .allowCredentials(true); // Allow sending credentials (e.g., cookies)
+    }
+}
